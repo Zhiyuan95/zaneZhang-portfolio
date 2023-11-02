82dca2c2cf2e867b1b90b67559e890eb99d45aa0
Zhiyuan Zhang <68951730+Zhiyuan95@users.noreply.github.com>
2023-11-02 15:36:34 +1000

    add download function

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import xxx from "../../../public/next.svg";
 
 const HeroSection = () => {
   return (
@@ -24,9 +25,7 @@ const HeroSection = () => {
               sequence={[
                 "Zane Zhang",
                 1000,
-                "Web Developer",
-                1000,
-                "Mobile Developer",
+                "Full-Stack Developer",
                 1000,
                 "UI/UX Designer",
                 1000,
@@ -37,8 +36,12 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptatum.
+            Experienced software engineer skilled in agile development,
+            specializing in building responsive web applications using
+            <span className="text-blue-900 font-bold">
+              React, TypeScript, Mobx, GraphQL, HTML/CSS, Node.js, and SQL.
+            </span>
+            Eager to collaborate, add value, and grow.
           </p>
           <div>
             <Link
@@ -48,10 +51,12 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="/ZaneZhang_Resume.pdf"
+              target="_blank"
+              download="ZaneZhang_Resume.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <span className="block bg-[#121212] hover-bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
             </Link>
